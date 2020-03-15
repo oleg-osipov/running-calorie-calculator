c731785905d3f25e133748bdf540dc8e5a5dbb7b
OlegOsipov <oleg-osipov@hotmail.com>
2020-03-16 00:10:18 +0100

    gh-pages

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../scss/main.scss';
-import '../assets/imgs/icon_sprite.svg';
-import rangeSlider from './range_slider.js';
-import toggleSwitch from './toggle_switch.js';
-import processData from './calorie_calculation.js';
-
-toggleSwitch();
-rangeSlider();
-processData();
